7ecf0b5250f5e9a6829e786434843d83bdeeb319
Bobuskiy <bobuskiy.olexander@example.com>
2024-08-13 15:09:14 +0200

    Add data sender

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,16 +1,54 @@
-import React from 'react'
+import { useState } from "react";
+import { useForm } from "@mantine/form"; // Adjust the import path according to your setup
 
 import "./Contact.scss";
 import { motion } from "framer-motion";
 import { animPage } from "../../helpers/animPage";
 
+import { sendMessage } from "../../requests/telegram";
+
 export default function Contact() {
+  const form = useForm({
+    initialValues: {
+      name: "",
+    },
+
+    // Validation functions for form fields
+    validate: {
+      name: (value) => (value ? null : "Name is required"),
+      // Add more validations as needed
+    },
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async ({ name }) => {
+    try {
+      setIsLoading(true);
+
+      const message = `Імʼя: ${name}`;
+      await sendMessage(message);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <motion.section {...animPage} className="container container">
       <div className="contact__title">
         <h1>Contact us</h1>
-        <span className="light">Always by your side, ready to answer any question</span>
+        <span className="light">
+          Always by your side, ready to answer any question
+        </span>
       </div>
+      <form className="form" onSubmit={form.onSubmit(handleSubmit)}>
+        <input type="text" placeholder="Name" {...form.getInputProps("name")} />
+        <button className="form-button" type="submit">
+          Submit
+        </button>
+      </form>
     </motion.section>
-  )
+  );
 }
---
+++ src/requests/telegram.js
@@ -0,0 +1,15 @@
+const baseUrl =
+  "https://api.telegram.org/bot7471723817:AAH17ACWov_YX3n3XKTUWa4LoCUhC8By7pg/";
+
+export const sendMessage = async (message) => {
+  const url = `${baseUrl}sendMessage?chat_id=-1002151428890&text=${message}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const error = await response.json();
+
+    await Promise.reject(error.description || "Щось не так(");
+  }
+
+  console.log("response", response);
+};
